4c84cb68221ac0a2696f66712b74e8937ec0ea14
Santoshsr07 <santoshsr7096@gmail.com>
2024-12-29 23:51:23 +0530

    enlarge

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// fetch header and footer
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header-placeholder').innerHTML = data);
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer-placeholder').innerHTML = data);
+
+
+    // 
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
